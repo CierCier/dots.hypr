45cbf0f89648129e3da0df199125519b5ffe7f5f
opticXC <aabishmalik3337@gmail.com>
2023-12-24 23:00:17 +0530

    hypr animations

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -3,7 +3,5 @@
 
 swayidle -w \
     timeout 300 'hyprctl dispatch dpms off' \
-    timeout 590 'gtklock' \
-    timeout 600 'hyprctl dispatch dpms off' \
     after-resume 'hyprctl dispatch dpms on'
 
